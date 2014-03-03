0fd45ed09642defb395d354f2ef65429bba79519
George Rogers <grogers385@gmail.com>
2014-03-03 09:34:11 -0600

    update

--- tests/001-css.rb
+++ tests/001-css.rb
@@ -1,7 +1,7 @@
 require './lib/brandaid/app'
 class TestCss < MiniTest::Test
   def test_to_css
-    rules = [[["h1"],
+    rules = [[[".primary"],
               [
                ['font-family', ["Helvetica", "Arial", "Droid Sans", "sans-serif"]],
               ]
