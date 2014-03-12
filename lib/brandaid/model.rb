49326139b152f4f9d89c3c2b52bcc29f1bff8410
George Rogers <grogers385@gmail.com>
2014-03-12 13:29:25 -0500

    update

--- lib/brandaid/model.rb
+++ lib/brandaid/model.rb
@@ -2,10 +2,10 @@ require 'moped'
 module BrandAid
   extend self
   def Session
-    url = URI.parse(ENV['MONGOHQ_URL'] || "localhost:27017/brandaid")
+    url = URI.parse(ENV['MONGOHQ_URL'] || "mongodb://localhost:27017/brandaid")
     sess = Moped::Session.new(["#{url.host}:#{url.port}"])
     sess.use(url.path.sub('/', ''))
-    sess.login(url.user, url.password)
+    sess.login(url.user, url.password) if url.user && url.password
     return sess
   end
   module Css
